ac6bef0f6859419baffdc3b2c7ada00c3b8ee27d
jigar.mistry <jigar.mistry@Bluekaktus.com>
2024-08-05 12:39:11 +0530

    Header component and Package handle issue solved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AgGridAngular, AgGridModule } from 'ag-grid-angular';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HeaderComponent } from './app/core/header/header.component';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { SharedComponentsModule } from './shared/shared-components/shared-components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedComponentsModule,
+    AgGridAngular,
+    AgGridModule,
+    MegaMenuModule,
+    BsDropdownModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ,NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
